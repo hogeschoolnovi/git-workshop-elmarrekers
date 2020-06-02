fae6d50dfaf6b7fa92d152c995744b338781f32f
Elmar Rekers <e.rekers@novi-edication.nl>
2020-06-02 11:35:10 +0200

    logincontroller

--- src/novi/basic/Main.java
+++ src/novi/basic/Main.java
@@ -3,6 +3,9 @@ package novi.basic;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Hello Elmar");
+
+        LoginController loginController = new LoginController();
     }
 }
